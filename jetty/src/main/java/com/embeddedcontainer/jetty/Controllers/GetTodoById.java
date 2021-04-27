e66436ddd451403d0601a1f3eb2388b96ed602ac
Anthony Salemo <ajssalemo@gmail.com>
2021-04-27 15:39:43 -0400

    added a controller to find a task by ID

---
+++ jetty/src/main/java/com/embeddedcontainer/jetty/Controllers/GetTodoById.java
@@ -0,0 +1,25 @@
+package com.embeddedcontainer.jetty.Controllers;
+
+import javax.persistence.EntityNotFoundException;
+import javax.validation.constraints.NotEmpty;
+
+import com.embeddedcontainer.jetty.Entity.TodoEntity;
+import com.embeddedcontainer.jetty.Repository.TodoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GetTodoById {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    // Find todo by ID
+    @GetMapping("/api/v1/todo/find/{id}")
+    public TodoEntity findTodoById(@PathVariable @NotEmpty Integer id) {
+        return todoRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Unable to find todo task with id: " + id));
+    }
+}
