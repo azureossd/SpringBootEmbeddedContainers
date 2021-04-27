a38cc61eee96aebaf066a9fa4f926808ded9d16b
Anthony Salemo <ajssalemo@gmail.com>
2021-04-27 11:15:19 -0400

    added a connection to postgres and an entity class

---
+++ jetty/src/main/java/com/embeddedcontainer/jetty/Entity/TodoEntity.java
@@ -0,0 +1,37 @@
+package com.embeddedcontainer.jetty.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "jettytodo")
+public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Boolean completed;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+}
