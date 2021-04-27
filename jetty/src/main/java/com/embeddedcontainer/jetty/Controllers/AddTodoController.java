1b6e44dfa2f1f6fd3f1940eeb0e5f392ded26a8a
Anthony Salemo <ajssalemo@gmail.com>
2021-04-27 15:41:30 -0400

    updated semantic naming of controller classes/filename

--- jetty/src/main/java/com/embeddedcontainer/jetty/Controllers/AddTodoController.java
+++ jetty/src/main/java/com/embeddedcontainer/jetty/Controllers/AddTodoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AddTodo {
+public class AddTodoController {
     @Autowired
     private TodoRepository todoRepository;
 
--- jetty/src/main/java/com/embeddedcontainer/jetty/Controllers/GetTodoByIdController.java
+++ jetty/src/main/java/com/embeddedcontainer/jetty/Controllers/GetTodoByIdController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GetTodoById {
+public class GetTodoByIdController {
     @Autowired
     private TodoRepository todoRepository;
 
