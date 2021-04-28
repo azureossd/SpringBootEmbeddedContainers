fb5f33a286ea1f52c910cb078ebc347ea5c5e3cd
Anthony Salemo <ajssalemo@gmail.com>
2021-04-28 10:51:36 -0400

    changed description, adding the start of the undertow example

---
+++ undertow/src/test/java/com/embeddedcontainer/undertow/UndertowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embeddedcontainer.undertow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndertowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
