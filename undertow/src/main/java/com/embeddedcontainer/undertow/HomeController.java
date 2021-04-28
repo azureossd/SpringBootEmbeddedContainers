d179f7e7ac7c2713bf14afca3221d805051e1329
Anthony Salemo <ajssalemo@gmail.com>
2021-04-28 14:58:28 -0400

    added a controller mapped to root

---
+++ undertow/src/main/java/com/embeddedcontainer/undertow/HomeController.java
@@ -0,0 +1,14 @@
+package com.embeddedcontainer.undertow;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    String message = "Spring boot running with an embedded Undertow container.";
+
+    @GetMapping("/")
+    public String home() {
+        return message;
+    }
+}
